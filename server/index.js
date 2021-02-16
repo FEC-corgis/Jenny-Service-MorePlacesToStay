ac917facb79d60f200ab9967cc5413cae19db163
Jenny Hou <jhou2012@gmail.com>
2021-02-16 12:41:19 -0500

    static serve test fix

--- server/index.js
+++ server/index.js
@@ -5,9 +5,12 @@ const db = require('../database/db.js')
 const dummyData = require('./dummyData.js')
 const cors = require('cors')
 const axios = require('axios')
+const path = require('path')
 
+app.get('/bundle.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/bundle.js'))
+})
 app.use('/rooms/:id', express.static('./client/dist'))
-// app.use(express.static('./client/dist'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
